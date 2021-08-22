0ff2fcd704cf3b6df6b5f209866a10dbdb868e13
Mohamed Agamy <agamy207@gmail.com>
2021-08-23 00:45:10 +0200

    commit auth

--- app/src/main/java/com/example/onlineshop/ui/LoginFragment.java
+++ app/src/main/java/com/example/onlineshop/ui/LoginFragment.java
@@ -2,15 +2,25 @@ package com.example.onlineshop.ui;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.onlineshop.R;
 
-public class LoginFragment extends Fragment {
+import org.jetbrains.annotations.NotNull;
+
+public class LoginFragment extends Fragment implements View.OnClickListener {
+    TextView mSignUp ;
+    private NavController navController;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -23,5 +33,34 @@ public class LoginFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull @NotNull View view, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        iniView(view);
+    }
+
+    private void iniView(View view)
+    {
+        mSignUp=view.findViewById(R.id.login_l);
+        mSignUp.setOnClickListener(this);
+
+        //navController to OnClick
+        navController = Navigation.findNavController(view);
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId())
+        {
+            case R.id.login_l:
+                navController.navigate(R.id.action_loginFragment2_to_signUpFragment3);
+                break;
+        }
+
     }
 }
--- app/src/main/java/com/example/onlineshop/ui/SignUpFragment.java
+++ app/src/main/java/com/example/onlineshop/ui/SignUpFragment.java
@@ -4,16 +4,25 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.constraintlayout.utils.widget.MotionLabel;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.onlineshop.R;
 
 
-public class SignUpFragment extends Fragment {
+public class SignUpFragment extends Fragment implements View.OnClickListener{
+    TextView mLogIn ;
+    private NavController navController;
+
+    public SignUpFragment() {
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -31,5 +40,28 @@ public class SignUpFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull @org.jetbrains.annotations.NotNull View view, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        iniView(view);
+    }
+
+    private void iniView(View view)
+    {
+        mLogIn=view.findViewById(R.id.login_s);
+        mLogIn.setOnClickListener(this);
+
+        //navController to OnClick
+        navController = Navigation.findNavController(view);
+
+
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId())
+        {
+            case R.id.login_s:
+                navController.navigate(R.id.action_signUpFragment3_to_loginFragment2);
+                break;
+        }
     }
 }
